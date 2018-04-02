c41ceeccc29b69ed4f17d7c68410d6fac3305614
Joapfel <kraemer_johannes@gmx.de>
2018-04-02 12:59:12 +0200

    added tfidf; bow window size 5

--- BOWApproach.py
+++ BOWApproach.py
@@ -1,4 +1,4 @@
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
 import sklearn.cluster.k_means_
 from sklearn.cluster.k_means_ import KMeans, MiniBatchKMeans
 import numpy as np
@@ -12,24 +12,36 @@ def readFile(words, path):
 
             for word in words:
                 if(word in tokens):
+                    idx = tokens.index(word)
+                    #bow of 5 (2 on the left | 2 on the right)
+                    rval.append(' '.join(tokens[idx-2:idx+3]))
+                    #print(tokens)
+                    #print(tokens[idx-2:idx+3])
+                    #print()
+
                     #use the whole line as bow
-                    rval.append(line.strip())
-                    continue
+                    #rval.append(line.strip())
+                    #continue
         return rval
 
 #list of bow's, each containing one of the fruits
-fruitsCorpus = readFile(['apple', 'banana', 'oranges', 'watermelons'], 'resources/corpora/OpenSubtitles/small/combined')
+fruitsCorpus = readFile(['apple', 'banana', 'oranges', 'watermelons'], 'resources/corpora/OpenSubtitles/small/combined2')
 
-workCorpus = readFile(['office', 'home', 'building', 'house'], 'resources/corpora/OpenSubtitles/small/combined')
+workCorpus = readFile(['office', 'home', 'building', 'house'], 'resources/corpora/OpenSubtitles/small/combined2')
 
-workCorpus = workCorpus[0:3000]
+#reduce the amount of the work corpus since its causing memory issues otherwise
+workCorpus = workCorpus[0:8000]
 
 #print(len(fruitsCorpus))
 #print(len(workCorpus))
+#print(fruitsCorpus)
+#print(workCorpus)
 
 corpus = np.append(fruitsCorpus, workCorpus)
 
 vectorizer = CountVectorizer()
+#vectorizer = TfidfVectorizer()
+#vectorizer = HashingVectorizer()
 X = vectorizer.fit(corpus)
 #print(vectorizer.get_feature_names())
 
@@ -46,8 +58,12 @@ np.set_printoptions(threshold=np.nan)
 
 f = kmeans.predict(Xfruits)
 print(f)
+print('contains number one x times: ', list(f).count(1))
+print('contains number zero x times: ', list(f).count(0))
 
 print('----------------------')
 
 w = kmeans.predict(Xwork)
 print(w)
+print('contains number one x times: ', list(w).count(1))
+print('contains number zero x times: ', list(w).count(0))
